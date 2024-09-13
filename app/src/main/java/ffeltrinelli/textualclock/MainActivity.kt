02586ea14d10611bacd9b029aeeb5064227a3d5c
Francesco Feltrinelli <44259532+ffeltrinelli@users.noreply.github.com>
2024-09-13 22:42:51 +0200

    Keep screen on and hide system bars

--- app/src/main/java/ffeltrinelli/textualclock/MainActivity.kt
+++ app/src/main/java/ffeltrinelli/textualclock/MainActivity.kt
@@ -1,6 +1,7 @@
 package ffeltrinelli.textualclock
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +11,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import ffeltrinelli.textualclock.ui.composable.TextualClock
 import ffeltrinelli.textualclock.ui.theme.TextualClockTheme
 
@@ -17,6 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        keepScreenOn()
         setContent {
             TextualClockTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -24,6 +28,20 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemBars()
+    }
+
+    private fun keepScreenOn() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+
+    private fun hideSystemBars() {
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 }
 
