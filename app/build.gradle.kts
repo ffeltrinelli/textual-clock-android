9490ff319f64a355354961a0bcdbae8846cd5ddd
ffeltrinelli <francescofel86@gmail.com>
2024-11-09 23:25:08 +0100

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "ffeltrinelli.textualclock"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ffeltrinelli.textualclock"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -34,8 +34,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "1.8"
